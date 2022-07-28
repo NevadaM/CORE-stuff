6f97b236d1a28e90a9f9f0d9d02f91cc504d1e52
Neil Majithia <neil.majithia@live.co.uk>
2022-07-28 15:24:22 +0100

    updated smoothing

--- smoothing.py
+++ smoothing.py
@@ -1,51 +1,40 @@
-###### how easy is consumption smoothing?
-# trying to model permanent income hypothesis
-# over 10 periods, try to keep consumption as smooth as possible (or overall utility as great as possible i guess)
-# mix of expected and unexpected shocks
-# 2 expected shocks info comes before game starts
-# between unexpected shocks in random periods, no shock is directly followed by another shock
-# on game start with expected shock info, player chooses initial consumption
-# on each period, shock or no shock, choose new consumption
-# results show income path, chosen consumption path, optimal consumption path
-# NO DISCOUNT RATE, NO RATE OF INTEREST (YET) - if there was, use euler eqn - if equal, Ct = r/1+r * Budget Constraint
-# bydget constraint = (1+r)A(t-1) + sum(1/(1+r)^i  *  y(t+i))
-# every period needs to report current debt as well
-# each period has assets (savings/debt)
-# period-specific change in assets = income - consumption
-# let's start with no assets
+## consumption smoothing 
+# takes income per period, creates its own random shocks expected and unexpected
+# returns optimal consumption path
+# so far only works with no impatience/discounting
+# suggestions - could make more simple to read by making expected shocks happen in first half, unexpected in second ?
 
-# make optimal first
-# expected shocks affect expected lifetime income
-# given no discount rate or rate of interest, just divide by periods to find initial consumption path
-# unexpected temporary shock with no rate of interest or discounting, consumption path moves up/down by half shock size and stays like that for next period to recuperate?
-# unexpected permanent shock causes permanent change in consumption 
 
 import random
 import numpy as np
 import matplotlib.pyplot as plt
 
 INCOME_PER_PERIOD = 100
-DELTA = 0
-R = 0
+R = 0.0
 PERIODS = 10
 ASSETS = 0
 
 #calculate lifetime income and use it to get consumption per period
-def getConsumption(assets: float, expectedincomepath: list, periodevaluated: int) -> float:
+def getConsumption(assets: float, expectedincomepath: list, periodevaluated: int, interestrate: float = R) -> float:
     restoflife = expectedincomepath[periodevaluated-1: ]
-    lifetimeincome = assets + sum(restoflife)
+    incomesum = 0
+    i = 0
+    for y in restoflife:
+        incomesum += (1 / ((1+interestrate) ** i)) * y
+        i += 1
+    lifetimeincome = ((1+interestrate) * assets) + incomesum
     consumptionpp = lifetimeincome / len(restoflife)
     return round(consumptionpp, 4)
 
 #generate four random numbers to be four different periods of shocks
-randps = random.sample(range(10), k=4)
+randps = random.sample(range(2, 11), k=3)
 #generate four random numbers to be four shock income values, expected or unexpected (making them divisible by five so they look nicer)
-randis = random.sample(range(0, 200, 5), k=4)
+randis = random.sample(range(75, 125, 5), k=3)
 
 
 #SHOCKS - {period: income}
 expected = {randps[0]: randis[0], randps[1]: randis[1]}
-unexpected = {randps[2]: randis[2], randps[3]: randis[3]}
+unexpected = {randps[2]: randis[2]} #randps[3]: randis[3]
 unexpectedperiods = list(unexpected.keys())
 
 #anticipated income path from p0
@@ -61,17 +50,41 @@ expectedincomepath = initialincomepath.copy()
 
 period = 1
 consumptionpath = []
-consumption = getConsumption(ASSETS, expectedincomepath, 1)
+consumption = getConsumption(ASSETS, expectedincomepath, period)
 while period <= PERIODS:
     if period in unexpectedperiods:
         expectedincomepath[period-1] = unexpected[period]
         assetspath = np.array(expectedincomepath[:period-1]) - np.array(consumptionpath)
-        assets = sum(assetspath)
+        assets = 0
+        for t in assetspath:
+            assets += t
+            assets *= (1+R)
         consumption = getConsumption(assets, expectedincomepath, period)
     consumptionpath.append(consumption)
     period += 1
 
+print(f'Rate of Interest: {R * 100}%')
 print('Initial income path: ', initialincomepath)
+print('Expected Shocks (period: shock income): ', expected)
+print('Unexpected Shocks (period: shock income): ', unexpected)
 print('Actual income path: ', expectedincomepath)
 print('Consumption Path: ', consumptionpath)
-print(round(sum(consumptionpath), 4))
+print(round(sum(consumptionpath)))
+
+## for the plot, trying to plot lines between each set of two points - (beginning of period, value) and (end of period, same value)
+## so a loop that plots point i to point i + 1
+## also need to plot vertical grey dashed jumps, between (end of period (period+1), ending value) and (beginning of next period (also period+1), new value)
+
+index = 0
+while index < (PERIODS):
+    plt.plot([index, index+1], [consumptionpath[index], consumptionpath[index]], c='r')
+    plt.plot([index, index+1], [expectedincomepath[index], expectedincomepath[index]], c='b')
+    plt.plot([index, index+1], [initialincomepath[index], initialincomepath[index]], c='g')
+    
+    if index != 9:
+        plt.plot([index+1, index+1], [consumptionpath[index], consumptionpath[index+1]], c='gray')
+        plt.plot([index+1, index+1], [expectedincomepath[index], expectedincomepath[index+1]], c='gray')
+        plt.plot([index+1, index+1], [initialincomepath[index], initialincomepath[index+1]], c='gray')
+    index+=1
+
+plt.show()
