b603e0609aa917b8e8f59529b2b585285d32cb40
Neil Majithia <neil.majithia@live.co.uk>
2022-07-29 17:13:34 +0100

    added discounting + previous assets

--- smoothing.py
+++ smoothing.py
@@ -1,8 +1,13 @@
 ## consumption smoothing 
 # takes income per period, creates its own random shocks expected and unexpected
 # returns optimal consumption path
-# so far only works with no impatience/discounting
-# suggestions - could make more simple to read by making expected shocks happen in first half, unexpected in second ?
+# so far only works with no interest rates
+# DOES WORK WITH DISCOUNTING THO
+# TODO        - DOESN'T WORK WITH INTEREST RATES - I think there's something wrong with evaluating consumption per period, iniitially decides to just save tons, then on expected shock, 
+                                                #  it reevaluates and realises yo I have loads of assets (maybe from interest?) and boosts c massively
+                                                #  can't determine why it does that - just builds assets even though it doesn't expect a shock
+                                                #  then spends all those assets in one go - especially easy to see when unexpected shock at period 10, size 10
+
 
 
 import random
@@ -10,31 +15,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 INCOME_PER_PERIOD = 100
-R = 0.0
+R = 0.01 ##do this as a float - e.g. if 1%, write 0.01
+DELTA = 0.01 ## same here
 PERIODS = 10
 ASSETS = 0
 
+# random.seed()
+
 #calculate lifetime income and use it to get consumption per period
-def getConsumption(assets: float, expectedincomepath: list, periodevaluated: int, interestrate: float = R) -> float:
+def getConsumption(assets: float, expectedincomepath: list, periodevaluated: int, interestrate: float = R, discountrate: float = DELTA) -> list:
     restoflife = expectedincomepath[periodevaluated-1: ]
     incomesum = 0
     i = 0
     for y in restoflife:
-        incomesum += (1 / ((1+interestrate) ** i)) * y
+        incomesum += (y / ((1+interestrate) ** i))
+        # incomesum += (y)
         i += 1
     lifetimeincome = ((1+interestrate) * assets) + incomesum
-    consumptionpp = lifetimeincome / len(restoflife)
-    return round(consumptionpp, 4)
+    c1 = lifetimeincome / len(restoflife)
+    # return round(consumptionpp, 4)
+    consumptionpath = []
+    j = 0
+    for y in restoflife:
+        consumptionpath.append(c1 * (((1+interestrate) / (1+discountrate)) ** j))
+        j+=1
+    return np.round(consumptionpath, 5)
 
-#generate four random numbers to be four different periods of shocks
-randps = random.sample(range(2, 11), k=3)
 #generate four random numbers to be four shock income values, expected or unexpected (making them divisible by five so they look nicer)
 randis = random.sample(range(75, 125, 5), k=3)
 
+#generate two random numbers to be 2 different expected shocks in first half
+randpsexpected = random.sample(range(2, 7), k=2)
+#and one random number for 1 unexpected shock, in second half
+randpsunexpected = random.sample(range(7, 11), k=1)
+
 
 #SHOCKS - {period: income}
-expected = {randps[0]: randis[0], randps[1]: randis[1]}
-unexpected = {randps[2]: randis[2]} #randps[3]: randis[3]
+expected = {randpsexpected[0]: randis[0], randpsexpected[1]: randis[1]}
+unexpected = {randpsunexpected[0]: randis[2]} #randps[3]: randis[3]
 unexpectedperiods = list(unexpected.keys())
 
 #anticipated income path from p0
@@ -49,18 +67,18 @@ for i in expected:
 expectedincomepath = initialincomepath.copy()
 
 period = 1
-consumptionpath = []
-consumption = getConsumption(ASSETS, expectedincomepath, period)
+# consumptionpath = []
+consumptionpath = getConsumption(ASSETS, expectedincomepath, period)
 while period <= PERIODS:
     if period in unexpectedperiods:
         expectedincomepath[period-1] = unexpected[period]
-        assetspath = np.array(expectedincomepath[:period-1]) - np.array(consumptionpath)
-        assets = 0
+        assetspath = np.array(expectedincomepath[:period-1]) - np.array(consumptionpath[:period-1])
+        assets = ASSETS * (1+R)
         for t in assetspath:
             assets += t
             assets *= (1+R)
-        consumption = getConsumption(assets, expectedincomepath, period)
-    consumptionpath.append(consumption)
+        consumptionpath[period-1:] = getConsumption(assets, expectedincomepath, period)
+    #consumptionpath.append(consumption)
     period += 1
 
 print(f'Rate of Interest: {R * 100}%')
