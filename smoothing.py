4aa94bbf8eb6b937724c9100852c575b9974d806
Neil Majithia <neil.majithia@live.co.uk>
2022-08-02 13:08:34 +0100

    created notebook, changed calc

--- smoothing.py
+++ smoothing.py
@@ -1,108 +1,85 @@
-## consumption smoothing 
-# takes income per period, creates its own random shocks expected and unexpected
-# returns optimal consumption path
-# so far only works with no interest rates
-# DOES WORK WITH DISCOUNTING THO
-# TODO        - DOESN'T WORK WITH INTEREST RATES - I think there's something wrong with evaluating consumption per period, iniitially decides to just save tons, then on expected shock, 
-                                                #  it reevaluates and realises yo I have loads of assets (maybe from interest?) and boosts c massively
-                                                #  can't determine why it does that - just builds assets even though it doesn't expect a shock
-                                                #  then spends all those assets in one go - especially easy to see when unexpected shock at period 10, size 10
-
-
-
 import random
-import numpy as np
 import matplotlib.pyplot as plt
 
-INCOME_PER_PERIOD = 100
-R = 0.00 ##do this as a float - e.g. if 1%, write 0.01
-DELTA = 0.01 ## same here
-PERIODS = 10
-ASSETS = 0
-
-# random.seed()
-
-#calculate lifetime income and use it to get consumption per period
-def getConsumption(assets: float, expectedincomepath: list, periodevaluated: int, interestrate: float = R, discountrate: float = DELTA) -> list:
+def getConsumption(assets: float, expectedincomepath: list, periodevaluated: int, interestrate: float) -> float:
+    print(assets)
     restoflife = expectedincomepath[periodevaluated-1: ]
-    incomesum = 0
-    i = 0
-    for y in restoflife:
-        incomesum += (y / ((1+interestrate) ** i))
-        # incomesum += (y)
-        i += 1
-    lifetimeincome = ((1+interestrate) * assets) + incomesum
+    incomesum = sum(restoflife)
+    lifetimeincome = assets + incomesum
     c1 = lifetimeincome / len(restoflife)
-    # return round(consumptionpp, 4)
-    consumptionpath = []
-    j = 0
+    interestsum = 0
     for y in restoflife:
-        consumptionpath.append(c1 * (((1+interestrate) / (1+discountrate)) ** j))
-        j+=1
-    return np.round(consumptionpath, 5)
+        interest = interestrate * (y - c1)
+        interestsum += interest
+    c1 += (interestsum / len(restoflife))
+    return round(c1, 4)
 
-#generate four random numbers to be four shock income values, expected or unexpected (making them divisible by five so they look nicer)
-randis = random.sample(range(75, 125, 5), k=3)
+class Smoothing():
+    def __init__(self, INCOME_PER_PERIOD, PERIODS, ASSETS, R) -> None:
+        self.PERIODS = PERIODS
+        self.R = R
 
-#generate two random numbers to be 2 different expected shocks in first half
-randpsexpected = random.sample(range(2, 7), k=2)
-#and one random number for 1 unexpected shock, in second half
-randpsunexpected = random.sample(range(7, 11), k=1)
+        randis = random.sample(range(75, 125, 5), k=3)
+        randpsexpected = random.sample(range(2, 7), k=2)
+        randpsunexpected = random.sample(range(7, 11), k=1)
+
+        expected = {randpsexpected[0]: randis[0], randpsexpected[1]: randis[1]}
+        unexpected = {randpsunexpected[0]: randis[2]} 
+        unexpectedperiods = list(unexpected.keys())
+        
+        initialincomepath = []
+        for i in range(PERIODS):
+            initialincomepath.append(INCOME_PER_PERIOD)
+        for i in expected:
+            initialincomepath[i-1] = expected[i]
+
+        expectedincomepath = initialincomepath.copy()
+
+        period = 1
+        assetpath = [ASSETS]
+        assets = ASSETS
+        consumptionpath = []
+        consumption = getConsumption(assets, expectedincomepath, period, R)
+        while period <= PERIODS:
+            assets = assets * (1+R)
+            if period in unexpectedperiods:
+                expectedincomepath[period-1] = unexpected[period]
+            consumption = getConsumption(assets, expectedincomepath, period, R)
+            assetpath.append(expectedincomepath[period-1] - consumption)
+            consumptionpath.append(consumption)
+            assets = sum(assetpath)
+            period += 1
+
+        self.initialincomepath = initialincomepath
+        self.expectedincomepath = expectedincomepath
+        self.consumptionpath = consumptionpath
+        self.assetpath = assetpath
+        self.expected = expected
+        self.unexpected = unexpected
 
 
-#SHOCKS - {period: income}
-expected = {randpsexpected[0]: randis[0], randpsexpected[1]: randis[1]}
-unexpected = {randpsunexpected[0]: randis[2]} #randps[3]: randis[3]
-unexpectedperiods = list(unexpected.keys())
+    def PrintStuff(self) -> None:
+        print(f'Rate of Interest: {self.R * 100}%')
+        print('Initial income path: ', self.initialincomepath)
+        print('Expected Shocks (period: shock income): ', self.expected)
+        print('Unexpected Shocks (period: shock income): ', self.unexpected)
+        print('Actual income path: ', self.expectedincomepath)
+        print('Consumption Path: ', self.consumptionpath)
+        print('Asset path: ', self.assetpath)
+        print('Sum of consumption: ', round(sum(self.consumptionpath)))
+        print('Sum of assets: ', round(sum(self.assetpath)))
 
-#anticipated income path from p0
-initialincomepath = []
-for i in range(PERIODS):
-    initialincomepath.append(INCOME_PER_PERIOD)
-for i in expected:
-    initialincomepath[i-1] = expected[i]
+    def PlotStuff(self):
+        index = 0
+        while index < (self.PERIODS):
+            plt.plot([index, index+1], [self.expectedincomepath[index], self.expectedincomepath[index]], c='b')
+            plt.plot([index, index+1], [self.initialincomepath[index], self.initialincomepath[index]], c='g')
+            plt.plot([index, index+1], [self.consumptionpath[index], self.consumptionpath[index]], c='r')
+            
+            if index != (self.PERIODS - 1):
+                plt.plot([index+1, index+1], [self.expectedincomepath[index], self.expectedincomepath[index+1]], c='gray')
+                plt.plot([index+1, index+1], [self.initialincomepath[index], self.initialincomepath[index+1]], c='gray')
+                plt.plot([index+1, index+1], [self.consumptionpath[index], self.consumptionpath[index+1]], c='gray')
+            index+=1
 
-
-
-expectedincomepath = initialincomepath.copy()
-
-period = 1
-# consumptionpath = []
-consumptionpath = getConsumption(ASSETS, expectedincomepath, period)
-while period <= PERIODS:
-    if period in unexpectedperiods:
-        expectedincomepath[period-1] = unexpected[period]
-        assetspath = np.array(expectedincomepath[:period-1]) - np.array(consumptionpath[:period-1])
-        assets = ASSETS * (1+R)
-        for t in assetspath:
-            assets += t
-            assets *= (1+R)
-        consumptionpath[period-1:] = getConsumption(assets, expectedincomepath, period)
-    #consumptionpath.append(consumption)
-    period += 1
-
-print(f'Rate of Interest: {R * 100}%')
-print('Initial income path: ', initialincomepath)
-print('Expected Shocks (period: shock income): ', expected)
-print('Unexpected Shocks (period: shock income): ', unexpected)
-print('Actual income path: ', expectedincomepath)
-print('Consumption Path: ', consumptionpath)
-print(round(sum(consumptionpath)))
-
-## for the plot, trying to plot lines between each set of two points - (beginning of period, value) and (end of period, same value)
-## so a loop that plots point i to point i + 1
-## also need to plot vertical grey dashed jumps, between (end of period (period+1), ending value) and (beginning of next period (also period+1), new value)
-
-index = 0
-while index < (PERIODS):
-    plt.plot([index, index+1], [consumptionpath[index], consumptionpath[index]], c='r')
-    plt.plot([index, index+1], [expectedincomepath[index], expectedincomepath[index]], c='b')
-    plt.plot([index, index+1], [initialincomepath[index], initialincomepath[index]], c='g')
-    
-    if index != 9:
-        plt.plot([index+1, index+1], [consumptionpath[index], consumptionpath[index+1]], c='gray')
-        plt.plot([index+1, index+1], [expectedincomepath[index], expectedincomepath[index+1]], c='gray')
-        plt.plot([index+1, index+1], [initialincomepath[index], initialincomepath[index+1]], c='gray')
-    index+=1
-
-plt.show()
+        plt.show()
